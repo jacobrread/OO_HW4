7836c5c4b7e1f69062691cdafaa78423208e7c1c
jacobread <jread8888@gmail.com>
2023-03-22 19:40:30 -0600

    Started implementation and added test project

--- hw4/Program.cs
+++ hw4/Program.cs
@@ -1,3 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
 
+namespace hw4
+{
+    class SudokuSolver
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: SudokuSolver <input file> <solver>");
+                Console.WriteLine("Solvers: BruteForceSolver, BacktrackingSolver, DancingLinksSolver");
+                return;
+            }
+
+            string inputFileName = args[0];
+            string solverName = args[1];
+
+            // Read the input file
+            String? line;
+            try
+            {
+                StreamReader sr = new StreamReader(inputFileName);
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            // TODO: create appropriate board
+            // int[,] board = new int[9, 9];
+
+            // TODO: create new solver based on the solverName argument
+        }
+
+        /// <summary>
+        /// Outputs the solution to a file
+        /// </summary>
+        public static void OutputSolution()
+        {
+            try
+            {
+                // TODO: Make this output location dynamic
+                StreamWriter sw = new StreamWriter("C/Users/jacobread/Desktop/OO/hw4/hw4/Output/output.txt");
+                //Write a line of text
+                sw.WriteLine("Hello World!!");
+                //Write a second line of text
+                sw.WriteLine("From the StreamWriter class");
+                //Close the file
+                sw.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
---
+++ hw4/SolverTemplate.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace hw4
+{
+	public abstract class SolverTemplate
+	{
+		private int[,] board { get; }
+		private int[,] solution { get; set; }
+
+		// Constructor
+		public SolverTemplate(int[,] board)
+		{
+			this.board = board;
+			this.solution = new int[board.Length, board.Length];
+		}
+
+		/// <summary>
+		/// Checks if a value is in a row
+		/// </summary>
+		/// <param name="row">The row to check</param>
+		/// <param name="value">The value to check for</param>
+		public bool CheckRow(int row, int value)
+		{
+			for (int i = 0; i < 9; i++)
+			{
+				if (board[row, i] == value)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// Checks if a value is in a column
+		/// </summary>
+		/// <param name="col">The column to check</param>
+		/// <param name="value">The value to check for</param>
+		public bool CheckCol(int col, int value)
+		{
+			for (int i = 0; i < 9; i++)
+			{
+				if (board[i, col] == value)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// Checks if a value is in a 3x3 box
+		/// </summary>
+		/// <param name="row">The row to check</param>
+		/// <param name="col">The column to check</param>
+		/// <param name="value">The value to check for</param>
+		public bool CheckBox(int row, int col, int value)
+		{
+			int rowStart = row - row % 3;
+			int colStart = col - col % 3;
+
+			for (int i = rowStart; i < rowStart + 3; i++)
+			{
+				for (int j = colStart; j < colStart + 3; j++)
+				{
+					if (board[i, j] == value)
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// Solves the board
+		/// </summary>
+		public abstract bool Solve();
+	}
+}
+
